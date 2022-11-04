e8e98e95716f52d4fed529bc4f3a79a8e2fddb0b
gimgitae <sux1011@naver.com>
2022-11-04 19:00:54 +0900

    2-11-04

---
+++ src/main/java/com/exampl/springbook/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package com.exampl.springbook.web.dto;
+
+import com.exampl.springbook.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
+}
---
+++ src/test/java/com/exampl/springbook/web/dto/HelloResponseDtoTes.java
@@ -0,0 +1,20 @@
+package com.exampl.springbook.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTes {
+
+    @Test
+    public void 롬복_기능_테스트() {
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
+}
