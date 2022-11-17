2b406f055502a1d4ef7bb282fb33aada49b97723
gimgitae <sux1011@naver.com>
2022-11-17 19:25:42 +0900

    무중단 배포 스크립트3

--- src/test/java/com/exampl/springbook/web/ProfileControllerTest.java
+++ src/test/java/com/exampl/springbook/web/ProfileControllerTest.java
@@ -23,14 +23,12 @@ public class ProfileControllerTest {
     private TestRestTemplate restTemplate;
 
     @Test
-    public void profil은_인증없이호출됨() throws Exception {
-
+    public void profile은_인증없이_호출된다() throws Exception {
         String expected = "default";
 
         ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isEqualTo(expected);
-
     }
-
 }
+
