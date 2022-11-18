b2f59d7d922384b1fddfc62b3a81ca061e951488
gimgitae <sux1011@naver.com>
2022-11-18 14:42:20 +0900

    무중단 배포 스크립트 .sh ver.4

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
-
 ABSPATH=$(readlink -f $0)
-ABSDIR=$(DIRNAME $ABSPATH)
+ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 function switch_proxy() {
     IDLE_PORT=$(find_idle_port)
 
     echo "> 전환할 Port: $IDLE_PORT"
-    echo "> Port 변환"
+    echo "> Port 전환"
     echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
     echo "> 엔진엑스 Reload"
-    sudo service nginx relod
+    sudo service nginx reload
 }
 
 #echo "set $service_url http://127.0.0.1:${IDLE_PORT};"
