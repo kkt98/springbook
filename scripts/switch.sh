c29abb8dc53bfe3e8970bd380f94adb016092965
gimgitae <sux1011@naver.com>
2022-11-18 11:14:26 +0900

    무중단 배포 스크립트 .sh

---
+++ scripts/switch.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(DIRNAME $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+function switch_proxy() {
+    IDLE_PORT=$(find_idle_port)
+
+    echo "> 전환할 Port: $IDLE_PORT"
+    echo "> Port 변환"
+    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+
+    echo "> 엔진엑스 Reload"
+    sudo service nginx relod
+}
+
+#echo "set $service_url http://127.0.0.1:${IDLE_PORT};"
+  #하나의 문장을 만들어 파이프라인(|)으로 넘겨주기 위해 echo를 사용
+  #엔진엑스가 변경할 프록시 주소를 생성
+#sudo tee /etc/nginx/conf.d/service-url.inc
+  #앞에서 넘겨준 문장을 service-url.inc에 덮어 씁니다.
+#sudo service nginx reload
+  #엔진엑스 설정을 다시 불러온다.
+  #restart는 잠시 끊기는 현상이 있지만, reload는 끊김 없이 다시 불러온다.
